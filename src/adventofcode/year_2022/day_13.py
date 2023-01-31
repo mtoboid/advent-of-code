bdc122129ae48396dd2aefb12d722039ee5e8323
Tobias Marczewski (mtoboid) <mtoboid@e.mail.de>
2023-01-31 17:09:19 +0100

    Day 13 part 2

--- src/adventofcode/year_2022/day_13.py
+++ src/adventofcode/year_2022/day_13.py
@@ -140,9 +140,55 @@ indices is 13.
 Determine which pairs of packets are already in the right order. What is the
 sum of the indices of those pairs?
 
+--- Part Two ---
+
+Now, you just need to put all of the packets in the right order. Disregard
+the blank lines in your list of received packets.
+
+The distress signal protocol also requires that you include two additional
+divider packets:
+
+[[2]]
+[[6]]
+
+Using the same rules as before, organize all packets - the ones in your list
+of received packets as well as the two divider packets - into the correct order.
+
+For the example above, the result of putting the packets in the correct order
+is:
+
+[]
+[[]]
+[[[]]]
+[1,1,3,1,1]
+[1,1,5,1,1]
+[[1],[2,3,4]]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[[1],4]
+[[2]]
+[3]
+[[4,4],4,4]
+[[4,4],4,4,4]
+[[6]]
+[7,7,7]
+[7,7,7,7]
+[[8,7,6]]
+[9]
+
+Afterward, locate the divider packets. To find the decoder key for this
+distress signal, you need to determine the indices of the two divider packets
+and multiply them together. (The first packet is at index 1, the second
+packet is at index 2, and so on.) In this example, the divider packets are
+10th and 14th, and so the decoder key is 140.
+
+Organize all of the packets into the correct order. What is the decoder key
+for the distress signal?
+
 """
 from __future__ import annotations
 
+from functools import cmp_to_key
 from json import loads as json_loads
 
 from adventofcode.challenge import DayChallenge, Path
@@ -165,18 +211,27 @@ class Day13(DayChallenge):
         with input_data.open() as file:
             data = file.read().split("\n")
 
-        signals: list[tuple[list, list]] = Day13.parse_input_pairs(data)
+        signals_pairs: list[tuple[list, list]] = Day13.parse_input_pairs(data)
 
 
         # PART 1
         print("Part 1:")
-        comparisons: list[int] = [Day13.compare(x[0], x[1]) for x in signals]
+        comparisons: list[int] = [Day13.compare(x[0], x[1])
+                                  for x in signals_pairs]
         indices_right_order = [idx+1 for idx in range(len(comparisons))
                                if comparisons[idx] == -1]
         print(f"The sum of the indices is: {sum(indices_right_order)}")
 
         # PART 2
         print("\nPart 2:")
+        signals = list([[[2]], [[6]]])
+        for pair in signals_pairs:
+            signals.extend(pair)
+        signals.sort(key=cmp_to_key(Day13.compare))
+        idx2 = signals.index([[2]]) + 1
+        idx6 = signals.index([[6]]) + 1
+        print(f"index of [[2]]: {idx2}, index of [[6]]: {idx6}")
+        print(f"decoder key = {idx2 * idx6}")
 
     @staticmethod
     def parse_input_pairs(lines: list[str]) -> list[tuple[list, list]]:
